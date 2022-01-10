d2f275b322a96df92a1c2bb3cc7daab7587b24f6
Cody <cody.tian@icloud.com>
2022-01-10 14:50:26 -0800

    add noStroke(), noCursor(), environment

--- sketch.js
+++ sketch.js
@@ -12,21 +12,33 @@ My own version of Apico. There will be at least some modifications.
     tree shadow
     generate_rock()
         ...
-    
+
 
 
  */
 let font
+let cursor
+let environment
+let bee
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
+    // load our cursor and environment
+    cursor = loadImage("img/apico-6x8-cursor.png")
+    environment = loadImage("img/environment-640x360.png")
+    textFont(font)
 }
 
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+    // no cursor and no stroke
+    noCursor()
+    noStroke()
 }
 
 function draw() {
     background(209, 80, 30)
+    image(cursor, mouseX, mouseY)
+    image(environment, 0, 0)
 }
